7459b479357521454cee60b735677882af2ab772
vivienneobrien <vivienneobrienis@gmail.com>
2024-06-17 18:54:13 -0700

    Added width from controls back in

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ import GUI from "lil-gui";
  */
 
 // Debug
-const gui = new GUI(); // { width: 200 }
+const gui = new GUI({ width: 360 });
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
